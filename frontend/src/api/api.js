5ec0862cb195e2c7142ddb718c03c0a0bef22185
patelchintan18018 <50191312+patelchintan18018@users.noreply.github.com>
2025-06-06 19:43:13 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  /*baseURL: 'http://localhost:5000/api', */
+  baseURL: 'https://mypcot-crud.vercel.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
